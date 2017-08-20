85ca934977a667add32fdbf1942fc7e9f26c18c5
Thomas Markovich <thomasmarkovich+git@gmail.com>
2017-08-19 21:06:23 -0400

    Added numba example

--- beta_logpdf/beta.py
+++ beta_logpdf/beta.py
@@ -1,18 +1,19 @@
 from _beta.lib import beta_logpdf, nop
-# from scipy.stats import beta
+import numba
+from scipy.stats import beta
 import time
 
 N = 1000000
-# start = time.time()
-# for i in range(N):
-    # a = beta.logpdf(i, 1, 2)
-# end = time.time()
-# print(end - start)
+start = time.time()
+for i in range(N):
+    a = beta.logpdf(i, 1, 2)
+end = time.time()
+print(end - start)
 
-# start = time.time()
-# a = beta.logpdf(list(range(N)), 1, 2)
-# end = time.time()
-# print(end - start)
+start = time.time()
+a = beta.logpdf(list(range(N)), 1, 2)
+end = time.time()
+print(end - start)
 
 start = time.time()
 for i in range(N):
@@ -25,3 +26,23 @@ for i in range(N):
     a = nop(1, 2, i)
 end = time.time()
 print(end - start)
+
+@numba.jit(nopython=True)
+def fn():
+    for i in range(N):
+        a = beta.logpdf(i, 1, 2)
+
+# Numba appears to fail here. Uncomment if you'd like to try
+# @numba.jit(nopython=True)
+# def fn_array():
+    # a = beta.logpdf(list(range(N)), 1, 2)
+
+# start = time.time()
+# fn()
+# end = time.time()
+# print(end - start)
+
+# start = time.time()
+# fn_array()
+# end = time.time()
+# print(end - start)
